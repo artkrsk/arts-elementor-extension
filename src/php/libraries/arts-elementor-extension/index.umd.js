5bc25561264ce4256f376346e268169e4c6a83bf
Artem Semkin <art.krsk@gmail.com>
2026-01-29 14:09:09 +0800

    update scripts

--- src/php/libraries/arts-elementor-extension/index.umd.js
+++ src/php/libraries/arts-elementor-extension/index.umd.js
@@ -1,16 +1,16 @@
 /*!
- * Arts Elementor Extension v1.0.5
+ * Arts Elementor Extension v1.0.7
  * https://artemsemkin.com
  * https://github.com/artkrsk/arts-elementor-extension
- * © 2025 Artem Semkin
+ * © 2026 Artem Semkin
  * License: MIT
  */
 (function(root, factory) {if (typeof define === 'function' && define.amd) {define(["jquery","elementor","backbone"], factory);} else if (typeof module === 'object' && module.exports) {module.exports = factory(require('jquery'), require('elementor'), require('backbone'));} else {root.ArtsElementorExtension = factory(root.jQuery, root.elementor, root.Backbone);}}(typeof self !== 'undefined' ? self : this, function(jQuery, elementor, Backbone) {
 /*!
- * Arts Elementor Extension v1.0.5
+ * Arts Elementor Extension v1.0.7
  * https://artemsemkin.com
  * https://github.com/artkrsk/arts-elementor-extension
- * © 2025 Artem Semkin
+ * © 2026 Artem Semkin
  * License: MIT
  */
 var ArtsElementorExtension=(()=>{var n=(r,e)=>{let t;return function(...i){clearTimeout(t),t=window.setTimeout(()=>r.apply(this,i),e)}};var o=class{constructor(){this.elementorInstance=null,this.previewWindow=null,this.resizeObserver=null,this.debouncedResizeEmit=null}init(e){return this.elementorInstance=e,this.elementorInstance.once("preview:loaded",()=>{this.setWindow(),this.addSettingsChangeCallbacks(),this.addReloadPreviewListener()}),{updatePreview:this.updatePreview.bind(this),reloadPreview:this.reloadPreview.bind(this),emitEvent:this.emitEvent.bind(this),disconnect:this.disconnect.bind(this)}}setWindow(){this.previewWindow=this.elementorInstance.$preview.get(0).contentWindow,this.initResizeObserver()}initResizeObserver(){if(!this.previewWindow)return;this.debouncedResizeEmit=n(t=>{let i=t[0];if(i){let{width:s,height:d}=i.contentRect;this.emitEvent("arts/elementor_extension/editor/preview_resized",{width:s,height:d,contentRect:i.contentRect})}},300),this.resizeObserver=new ResizeObserver(this.debouncedResizeEmit),this.previewWindow.document&&this.previewWindow.document.body&&this.resizeObserver.observe(this.previewWindow.document.body);let e=document.getElementById("elementor-preview");e&&this.resizeObserver.observe(e)}disconnectResizeObserver(){this.resizeObserver&&(this.resizeObserver.disconnect(),this.resizeObserver=null),this.debouncedResizeEmit=null}addSettingsChangeCallbacks(){let{addChangeCallback:e}=this.elementorInstance.settings.page;(window.artsElementorExtensionEditorLiveSettings||[]).forEach(i=>e(i,s=>this.onSettingChange(i,s)))}onSettingChange(e,t){let i=this.elementorInstance.settings.page.model.attributes;this.emitEvent("arts/elementor_extension/editor/setting_changed",{settings:i,setting:e,value:t})}addReloadPreviewListener(){this.previewWindow&&(this.previewWindow.removeEventListener("arts/elementor_extension/editor/reload_preview",this.onReloadPreview.bind(this)),this.previewWindow.addEventListener("arts/elementor_extension/editor/reload_preview",this.onReloadPreview.bind(this)))}onReloadPreview(e){let t=e.detail||{},{route:i,section:s}=t;this.reloadPreview(i,s)}updatePreview(e,t){this.elementorInstance.once("preview:loaded",()=>{window.$e.data.get("globals/index").then(()=>{setTimeout(()=>{window.$e.routes.current.panel.search("panel/global")>=0?window.$e.run("panel/global/open").then(()=>{e&&window.$e.route(e),window.elementorCommon.elements.$body.removeClass("elementor-panel-loading")}):(e&&window.$e.route(e),t&&(this.elementorInstance.getPanelView().getCurrentPageView().activateSection(t),this.elementorInstance.getPanelView().getCurrentPageView().openActiveSection(),this.elementorInstance.getPanelView().getCurrentPageView().render()),window.elementorCommon.elements.$body.removeClass("elementor-panel-loading"))},500)})}),window.$e.run("preview/reload")}reloadPreview(e,t){window.elementorCommon.elements.$body.addClass("elementor-panel-loading"),window.$e.run("document/save/update").then(()=>{this.updatePreview(e,t)})}emitEvent(e,t){if(this.previewWindow){let i=new CustomEvent(e,{detail:t});this.previewWindow.dispatchEvent(i)}}disconnect(){this.disconnectResizeObserver()}},a={init:r=>new o().init(r)};window.addEventListener("elementor/init",()=>{a.init(window.elementor)});})();
